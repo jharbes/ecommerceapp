b0fbf8060995904821d760d0b77837b8f5adde2e
Jorge Harbes <jharbes@hotmail.com>
2023-01-25 11:36:40 -0300

    OrderStatus enum

--- src/main/java/com/jharbes/ecommerceapp/entities/Order.java
+++ src/main/java/com/jharbes/ecommerceapp/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jharbes.ecommerceapp.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,6 +30,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
+	private Integer orderStatus;
+
 	@ManyToOne // relacionamento muitos para um com user conforme consta no modelo conceitual
 	@JoinColumn(name = "client_id") // indicando o nome da chave estrangeira a ser utilizada
 	private User client;
@@ -36,10 +39,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		this.setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -65,6 +69,15 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
