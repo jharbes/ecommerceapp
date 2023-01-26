92f4a9ac442ff420468c834f2820405241f1b425
Jorge Harbes <jharbes@hotmail.com>
2023-01-26 14:48:57 -0300

    tratamento de excecao - findById

--- src/main/java/com/jharbes/ecommerceapp/services/UserService.java
+++ src/main/java/com/jharbes/ecommerceapp/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jharbes.ecommerceapp.entities.User;
 import com.jharbes.ecommerceapp.repositories.UserRepository;
+import com.jharbes.ecommerceapp.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
