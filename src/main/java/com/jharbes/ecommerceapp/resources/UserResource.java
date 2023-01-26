999045f60147ceec4b8175224bcc159d145056cb
Jorge Harbes <jharbes@hotmail.com>
2023-01-26 10:54:23 -0300

    insercao de User

--- src/main/java/com/jharbes/ecommerceapp/resources/UserResource.java
+++ src/main/java/com/jharbes/ecommerceapp/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.jharbes.ecommerceapp.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jharbes.ecommerceapp.entities.User;
 import com.jharbes.ecommerceapp.services.UserService;
@@ -30,7 +34,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 
-		return ResponseEntity.ok(obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) { // a notacao @RequestBody fara a conversao do json para
+																// o objeto User no java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
